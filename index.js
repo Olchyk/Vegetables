0e859207f8fc1bdd350e69ce298a72bb545d2dbe
Olha Kalytyn <olhakalytyn@gmail.com>
2025-03-19 14:32:49 +0200

    add some dklkfdfk

--- index.js
+++ index.js
@@ -69,5 +69,5 @@ const server = http.createServer((req, res) => {
 module.exports = server; // here is connecting the server with testing
 
 server.listen(8000, '127.0.0.1', () => {
-  console.log('Listening to requests on port 8000');
+  console.log('Listening to requests on port 8000'); // here is connecting the server with testing
 });
