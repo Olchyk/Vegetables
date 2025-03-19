23f058ad616a67a20bfc1487275febdfb25392a6
Olha Kalytyn <olhakalytyn@gmail.com>
2025-03-19 14:31:42 +0200

    add some comment

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ const server = http.createServer((req, res) => {
       'Content-type': 'text/html',
       'my-own-header': 'hello-world',
     });
-    res.end('<h1>Page not found!</h1>');
+    res.end('<h1>Page not found!</h1>'); // here is connecting the server with testing
   }
 });
 module.exports = server; // here is connecting the server with testing
